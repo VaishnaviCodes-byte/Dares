519c64b3357527dc52be5b30a531ec67d0464b1a
VaishnaviCodes-byte <vaishnavibaragali@gmail.com>
2026-07-10 19:19:14 +0530

    update login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Login.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://dares-zdxx.onrender.com";
 
 export function LoginSignup() {
   const [isLogin, setIsLogin] = useState(true);
